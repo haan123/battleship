482565cafaa799885c7125bdc00a70255f198e24
An Ha <an.ha@amblique.com>
2018-06-29 15:19:29 +0700

    ramdom arrange and animation

---
+++ src/core/animate.js
@@ -0,0 +1,94 @@
+/* eslint no-plusplus: 0 */
+/* eslint comma-dangle: 0 */
+/* eslint class-methods-use-this: 0 */
+/* eslint consistent-return: 0 */
+/* eslint array-callback-return: 0 */
+
+const Animate = {
+  init(config) {
+    const {
+      elem,
+      image,
+      imgWidth,
+      frameWidth,
+      frameHeight,
+      numberOfFrames,
+      ext
+    } = config;
+
+
+    const boardContainer = document.getElementById('board-container');
+    const clone = image.cloneNode(true);
+
+    clone.style.height = `${frameHeight}px`;
+    clone.style.width = `${frameWidth}px`;
+
+    const containerRect = boardContainer.getBoundingClientRect();
+    const rect = elem.getBoundingClientRect();
+
+    boardContainer.appendChild(clone);
+
+    clone.style.left = `${Math.floor((rect.left - containerRect.left) + ext.dx)}px`;
+    clone.style.top = `${Math.floor((rect.top - containerRect.top) + ext.dy)}px`;
+
+    const obj = this.sprite({
+      elem,
+      image: clone,
+      imgWidth,
+      numberOfFrames
+    });
+
+    this.animate(obj);
+  },
+
+  sprite(options) {
+    const {
+      image,
+      imgWidth,
+      loop,
+      ticksPerFrame = 4,
+      numberOfFrames = 1
+    } = options;
+
+    let frameIndex = 0;
+    let tickCount = 0;
+    const that = this;
+
+    return {
+      image,
+      update() {
+        tickCount += 1;
+
+        if (tickCount > ticksPerFrame) {
+          tickCount = 0;
+
+          if (frameIndex < numberOfFrames - 1) {
+            frameIndex += 1;
+          } else if (loop) {
+            frameIndex = 0;
+          }
+        }
+      },
+      render() {
+        if (frameIndex === numberOfFrames - 1) {
+          window.cancelAnimationFrame(that.req);
+        }
+
+        // Draw the animation
+        image.style.backgroundPosition = `-${(frameIndex * imgWidth) / numberOfFrames}px 0px`;
+      }
+    };
+  },
+
+  animate(obj) {
+    this.req = window.requestAnimationFrame(() => {
+      this.animate(obj);
+    });
+
+    obj.update();
+    obj.render();
+  }
+};
+
+
+export default Animate;
